2e7fe6f61ae87d729f1da2d45b87edbad7636a8b
PADMASHREE25 <79974564+PADMASHREE25@users.noreply.github.com>
2021-03-03 16:06:31 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,34 @@
+num1 = 35
+num2 = 45
+
+
+def add(a, b):
+    res = a+b
+    print(res)
+    return res
+
+
+def sub(a, b):
+    diff = a-b
+    print(diff)
+    return diff
+
+
+def mul(a, b):
+    prod = a*b
+    print(prod)
+    return prod
+
+
+def div(a, b):
+    quot = a/b
+    print(quot)
+    return quot
+
+
+add(num1, num2)
+sub(num1, num2)
+mul(num1, num2)
+div(num1, num2)
+
+print(sum)
